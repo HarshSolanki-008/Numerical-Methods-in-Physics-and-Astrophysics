4c208deb27c3b2f0718e6c5e0cbeb328cbc5a2eb
Harsh Solanki <139675852+PiBonD008@users.noreply.github.com>
2024-11-08 21:35:27 +0100

    Update

--- Exercie 03/Linear_Systems_Exercise.py
+++ Exercie 03/Linear_Systems_Exercise.py
@@ -9,3 +9,24 @@ def mat_mul(m, n):
     return Matrix
 Matrix = mat_mul(m,n)
 print(Matrix)
+
+
+def get_index(i, j, N):
+    return i * N + j
+
+def back_sub(M, b, N):
+    x = np.zeros(N)
+    for i in range(N - 1, -1, -1):
+        x[i] = b[i]
+        for j in range(i + 1, N):
+            x[i] -= M[get_index(i, j, N)] * x[j]
+        
+        x[i] /= M[get_index(i, i, N)]
+    
+    return x
+
+N = 3
+M = np.array([1, 2, 3, 0, 5, 6, 0, 0, 9], dtype=float)
+b = np.array([10, 11, 12], dtype=float)
+x = back_sub(M, b, N)
+print("Solution vector x:", x)
